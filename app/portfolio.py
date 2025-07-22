0119f6ee58dfb4673ea9ee4908a17a3187bed90f
MayurPatil4313 <mayurvijaypatil2002@gmail.com>
2025-07-22 15:32:13 +0530

    solving the error
    An Error Occurred: dict.get() takes no keyword arguments

--- app/portfolio.py
+++ app/portfolio.py
@@ -6,34 +6,41 @@ import os
 import chromadb
 
 class Portfolio:
-    def __init__(self , file_path='app/resource/generated_techstack_portfolio.csv'):
+    def __init__(self , file_path='app/resource/my_portfolio.csv'):
         self.file_path = file_path
         self.df = pd.read_csv(file_path)
         self.chroma_client = chromadb.PersistentClient('Vectorstore')
         self.collection = self.chroma_client.get_or_create_collection(name='portfolio')
 
     def load_portfolio(self):
-        print('inside load_portfolio')
-        print('db name ', self.chroma_client.list_collections())
-        # os.add_dll_directory(r"C:\Users\mayur\AppData\Local\Programs\Python\Python311\Lib\site-packages\onnxruntime")
-        print('adding onnxruntime')
+        # print('inside load_portfolio')
+        # print('db name ', self.chroma_client.list_collections())
+        # # os.add_dll_directory(r"C:\Users\mayur\AppData\Local\Programs\Python\Python311\Lib\site-packages\onnxruntime")
+        # print('adding onnxruntime')
 
         if not self.collection.count():
-            print('inside the collection present condation')
-            documents = self.df["Techstack"].tolist()
-            metadatas = [{"Links": link} for link in self.df["Links"]]
-            ids = [str(i) for i in self.df.index]
+            for _, row in self.df.iterrows():
+                self.collection.add(documents=row["Techstack"],
+                                    metadatas={"links": row["Links"]},
+                                    ids=[str(uuid.uuid4())])
 
-            # Add data to the collection
-            self.collection.add(
-                documents=documents,
-                metadatas=metadatas,
-                ids=ids
-            )
+            # print('inside the collection present condation')
+            # documents = self.df["Techstack"].tolist()
+            # metadatas = [{"Links": link} for link in self.df["Links"]]
+            # ids = [str(i) for i in self.df.index]
+
+            # # Add data to the collection
+            # self.collection.add(
+            #     documents=documents,
+            #     metadatas=metadatas,
+            #     ids=ids
+            # )
+
     def query_links(self, skills):
         print('qury executing in query_links')
         print('skills in query_links => ', skills)
-        return self.collection.query(query_texts=skills, n_results=2).get(metadatas='metadatas')
+        # return self.collection.query(query_texts=skills, n_results=2).get('metadatas')
+        return self.collection.query(query_texts=skills, n_results=2).get('metadatas', [])
 
 
 if __name__ == "__main__":
